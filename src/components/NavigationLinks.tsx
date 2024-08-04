f7a6f92148bdc9b789ac90b57384183a9271033a
Eyad Ahmed <Eyad932004@outlook.com>
2024-08-04 11:57:20 +0300

    [0002]-finish navBar component

---
+++ src/components/NavigationLinks.tsx
@@ -0,0 +1,13 @@
+import Link from "./Link";
+
+const NavigationLinks = () => {
+  return (
+    <div className="grid grid-cols-3 col-span-3  gap-[1px]">
+      <Link to="." label="_hello" />
+      <Link to="./about" label="_about-me" />
+      <Link to="./projects" label="_projects" />
+    </div>
+  );
+};
+
+export default NavigationLinks;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,18 @@
+import { Code } from "astro:components";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        background: "#1E2D3D",
+        background: "#011627",
+        Code: "#607B96",
+        var: "#4D5BCE",
+        btn: "#FEA55F",
+      },
+      fontFamily: {
+        fira: ['"Fira Code"', "monospace"],
       },
     },
   },
