ac401d7f75da8e26887c4034131505c75f14d9ae
Eyad Ahmed <Eyad932004@outlook.com>
2024-08-17 20:51:01 +0300

    [0020]-edit index content

--- src/Store.js
+++ src/Store.js
@@ -1,7 +1,7 @@
 import { atom } from "nanostores";
 
 export const name = atom("eyad ahmed");
-export const position = atom("front-end developer");
+export const position = atom("front-end web developer");
 export const bio = atom(
   "I am a front-end developer with a passion for creating user-friendly and visually appealing interfaces. I have a strong background in React, Astro, NextJs and I am always looking for ways to improve my skills and stay up-to-date with the latest technologies.",
 );
